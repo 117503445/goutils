c7a771d5183673c71a1036fe121bf4fa4dd50ebc
117503445 <t117503445@gmail.com>
2024-09-17 02:48:56 +0800

    FindGitRepoRoot
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- common.go
+++ common.go
@@ -1,6 +1,9 @@
 package goutils
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/google/uuid"
@@ -18,4 +21,22 @@ func TimeStrMilliSec() string {
 
 func UUID4() string {
 	return uuid.New().String()
+}
+
+// FindGitRepoRoot
+func FindGitRepoRoot() (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	p := wd
+	for {
+		if _, err := os.Stat(p + "/.git"); err == nil {
+			return p, nil
+		}
+		if p == "/" {
+			return "", fmt.Errorf("Git repo root not found")
+		}
+		p = filepath.Dir(p)
+	}
 }
--- common_test.go
+++ common_test.go
@@ -17,4 +17,9 @@ func TestCommon(t *testing.T) {
 	log.Debug().Str("TimeStrSec", goutils.TimeStrSec()).Str("TimeStrMilliSec", goutils.TimeStrMilliSec()).Msg("Time")
 
 	log.Debug().Str("UUID4", goutils.UUID4()).Msg("UUID4")
+
+	dir, err := goutils.FindGitRepoRoot()
+	ast.NoError(err)
+	log.Debug().Str("GitRepoRoot", dir).Msg("GitRepoRoot")
+
 }
